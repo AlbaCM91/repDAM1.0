cf267417f633201cff1cdea0846ccbc2a5f4b8e8
'Alba' <alba.cardador-martin@iessampedro.org>
2022-03-17 19:27:43 +0100

    version 2 nuevo ejemplo

---
+++ PruebasGit1.0/src/probandoGit/Ejemplo2.java
@@ -0,0 +1,12 @@
+package probandoGit;
+
+public class Ejemplo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("probando comparacion del historial");
+		
+	}
+
+}
